c7d0226520f9eccf4dfe3c093c7a553a7e12fbf2
LittleYang0531 <littleyang0531@pop-os.localdomain>
2023-10-29 13:54:29 +0800

    Update

--- engine/data/particle/RedTap.cpp
+++ engine/data/particle/RedTap.cpp
@@ -14,12 +14,17 @@ class RedTap: public Archetype {
 	};
 
 	const var ParticleLength = 0.5;
-	var wt = judgline.rtX - judgline.ltX, wb = judgline.rbX - judgline.lbX - 2 * judglineMoveLength;
-	var lbx = (lane - 1) / 12 * wb + judgline.lbX + judglineMoveLength, lby = judgline.lbY;
-	var ltx = (lane - 1) / 12 * wt + judgline.ltX, lty = judgline.ltY;
-	var rtx = enLane / 12 * wt + judgline.ltX, rty = lty;
-	var rbx = enLane / 12 * wb + judgline.lbX + judglineMoveLength, rby = lby;
-	var mt = JudgelineTopHeight / 2, mb = JudgelineBottomHeight / 2;
+	var wt = judgline.rtX - judgline.ltX, wb = judgline.rbX - judgline.lbX;
+	var jh = judgline.ltY - judgline.lbY, h = 10.0 / 72.0 * jh;
+	var lp = 92.5 / 1119.0 * (lane - 1), rp = 92.5 / 1119.0 * enLane + 9.0 / 1119.0;
+	var ltx = judgline.ltX + lp * wt, lty = judgline.ltY;
+	var lbx = judgline.lbX + lp * wb, lby = judgline.lbY;
+	var rtx = judgline.ltX + rp * wt, rty = judgline.rtY;
+	var rbx = judgline.lbX + rp * wb, rby = judgline.rbY;
+	var adt = 4.0 / 1119.0 * wt, adb = 4.0 / 1119.0 * wb;
+	var adh = 4.0 / 72.0 * jh;
+
+	var LinearHeight = 0.5;
 	
 	var inCubic(var x) { return Ease(x, RuntimeFunction.EaseInCubic); };
 	var outCubic(var x) { return Ease(x, RuntimeFunction.EaseOutCubic); };
@@ -27,9 +32,13 @@ class RedTap: public Archetype {
 	var aOutCubic = outCubic(1 - (times.now - SpawnTime.get()) / ParticleLength);
 
 	var updateSequential = {
-		Draw(Sprites.HorizontalLine, ltx, lty - 2 * mt, ltx, lty, rtx, rty, rtx, rty - 2 * mt, 1e6, aInCubic),
-		Draw(Sprites.HorizontalLine, lbx, lby, lbx, lby + 2 * mb, rbx, rby + 2 * mb, rbx, rby, 1e6, aInCubic),
-		Draw(Sprites.VerticalLine, lbx - mb, lby, ltx - mt, lty, ltx + mt, lty, lbx + mb, lby, 1e6, aInCubic)
+		Draw(Sprites.HorizontalLine, ltx + adt, lty - h, ltx + adt, lty, rtx - adt, rty, rtx - adt, rty - h, 1e8, aInCubic),
+		Draw(Sprites.HorizontalLine, lbx + adb, lby, lbx + adb, lby + h, rbx - adb, rby + h, rbx - adb, rby, 1e8, aInCubic),
+		Draw(Sprites.VerticalLine, lbx, lby + adh, ltx, lty - adh, ltx + h, lty - adh, lbx + h, lby + adh, 1e8, aInCubic),
+		Draw(Sprites.VerticalLine, rbx - h, rby + adh, rtx - h, rty - adh, rtx, rty - adh, rbx, rby + adh, 1e8, aInCubic),
+		Draw(Sprites.LinearLine, lbx, lby, lbx, lby + LinearHeight, ltx, lty + LinearHeight, ltx, lty, 1e8, aInCubic),
+		Draw(Sprites.LinearLine, rbx, rby, rbx, rby + LinearHeight, rtx, rty + LinearHeight, rtx, rty, 1e8, aInCubic),
+		Draw(Sprites.LinearLine, ltx, lty, ltx, lty + LinearHeight, rtx, rty + LinearHeight, rtx, rty, 1e8, aInCubic),
 	};
 
 	var updateParallel = {
